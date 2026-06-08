81379797c82ca7dcb9128a3c14c47dc93e18fa90
peter <peter.szaflarski@gmail.com>
2026-06-08 17:28:28 +0000

    chore: change default machine type to n2-standard-2 in manage_tau_instance.sh

--- scripts/manage_tau_instance.sh
+++ scripts/manage_tau_instance.sh
@@ -16,7 +16,7 @@ PROJECT_ID="${PROJECT_ID:-tavern-swiper-dev}"
 ZONE="${ZONE:-northamerica-northeast2-b}"
 INSTANCE_NAME="${INSTANCE_NAME:-tau-ephemeral-instance}"
 GCS_BUCKET_PATH="${GCS_BUCKET_PATH:-gs://tavern-swiper-vm-backups}"
-MACHINE_TYPE="${MACHINE_TYPE:-n2-highmem-8}"
+MACHINE_TYPE="${MACHINE_TYPE:-n2-standard-2}"
 BOOT_DISK_SIZE="${BOOT_DISK_SIZE:-50GB}"
 EXPORT_ZONE="${EXPORT_ZONE:-us-central1-a}"
 
