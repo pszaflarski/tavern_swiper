03fabf4dfd0c4f560c97d4576ded9f900979dc5a
peter <peter.szaflarski@gmail.com>
2026-07-08 03:48:00 +0000

    fix(frontend): explicitly add babel transform plugins for logical assignment, nullish coalescing, and optional chaining

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,5 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      '@babel/plugin-transform-logical-assignment-operators',
+      '@babel/plugin-transform-nullish-coalescing-operator',
+      '@babel/plugin-transform-optional-chaining',
+    ],
   };
 };
